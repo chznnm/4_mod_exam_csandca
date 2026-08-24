f170132c33d39b352b28e5a4c44e98145013dc08
Aleksej <aleshikinterpriset@gmail.com>
2026-08-24 17:04:23 +0300

    fix 7 mod

--- pages/movie_page.py
+++ pages/movie_page.py
@@ -1,5 +1,4 @@
 from pages.base_page import BasePage
-from utils.data_generator import DataGenerator
 class MovieBasePage(BasePage):
     def __init__(self, page):
         super().__init__(page)
@@ -7,9 +6,14 @@ class MovieBasePage(BasePage):
         self.review_input = '[data-qa-id="movie_review_input"]'
         self.send_button = '[data-qa-id="movie_review_submit_button"]'
         self.rating_input =  '[data-qa-id="movie_rating_select"]'
+        self.actions_button = '[data-qa-id="movie_review_actions_button"]'
 
     def create_review(self,review_data,review_rating):
         self.enter_text(self.review_input, review_data)
         self.click(self.rating_input)
         self.page.get_by_role("option", name=review_rating).click()
         self.click(self.send_button)
+
+    def delete_review(self):
+        self.click(self.actions_button)
+        self.page.get_by_role("menuitem", name="Удалить").click()
